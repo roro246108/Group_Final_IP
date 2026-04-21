b23c26a0ce8202a030dcbe9f416735d06989efcd
Bata246613 <fatma246613@bue.edu.eg>
2026-04-21 05:34:49 +0200

    Authantication

--- Group/backend/MiddleWares/index.js
+++ Group/backend/MiddleWares/index.js
@@ -1,9 +1,8 @@
 import cors from "cors";
 import express from "express";
-import reviewRoutes from "../routes/review.js";
 
 export const globalMiddlewares = (app) => {
   app.use(cors());
   app.use(express.json());
-  app.use("/api/reviews", reviewRoutes);
+  app.use(express.urlencoded({ extended: true }));
 };
